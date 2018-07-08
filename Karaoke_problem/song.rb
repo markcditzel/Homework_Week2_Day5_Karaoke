f1d97ef58268e48826f183fc98096f20bce43b42
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-07-08 21:54:12 +0100

    Song class created and tests rirun

--- Karaoke_problem/song.rb
+++ Karaoke_problem/song.rb
@@ -1,2 +1,11 @@
 class Song
+
+  attr_reader :name, :artist, :genre
+
+def initialize(input_song_title, input_artist_name, input_genre)
+  @name = input_song_title
+  @artist = input_artist_name
+  @genre = input_genre
+end
+
 end
--- Karaoke_problem/specs/song_specs.rb
+++ Karaoke_problem/specs/song_specs.rb
@@ -1,6 +1,26 @@
-require('minitest/autorun')
-require('minitest/rg')
-require_relative('')
+require("minitest/autorun")
+require_relative("../song.rb")
 
-class SongTest < MiniTest::Test
+class SongTest<MiniTest::Test
+
+  def setup
+    @song1 = Song.new("I'm Happy", "Prince", "Pop")
+  end
+
+  def test_can_a_song_object_be_created
+    song1 = Song.new("adasd", "dasdas", "sfasdf")
+  end
+
+  def test_add_iv_title_to_song_object
+    assert_equal("I'm Happy", @song1.name)
+  end
+
+  def test_add_iv_artist_to_song_object
+    assert_equal("Prince", @song1.artist)
+  end
+
+  def test_add_iv_genre_to_song_object
+    assert_equal("Pop", @song1.genre)
+  end
+
 end
